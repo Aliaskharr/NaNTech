acda96d1d31f3de2ffa52fae560e7b667728c3cb
Aliaskhar <alizade.2403@gmail.com>
2025-03-13 22:25:07 +0300

    axios get and post part 2

--- React/Lessons/Axios/src/components/Courses.jsx
+++ React/Lessons/Axios/src/components/Courses.jsx
@@ -7,10 +7,10 @@ class Courses extends Component {
 
     state = {
         courses: null,
-        currerntCourse : {
-            
-        }
-        }
+        courseId: null,
+        courseName: null,
+        courseDesc: null
+    }
 
     componentDidMount() {
         axios.get('https://fakestoreapi.com/products').then(res => {
@@ -21,8 +21,23 @@ class Courses extends Component {
 
     addCourse = (event) => {
         event.preventDefault();
+        console.log(this.state.courseName);
+        console.log(this.state.courseDesc);
+        axios.post('https://fakestoreapi.com/products', {
+            courseName: this.state.courseName,
+            courseDesc: this.state.courseDesc
+        }).then(res => {
+            console.log(res);
+            
+        }).catch(err => {
+            console.log(err);
+            
+        })
         console.log('clicked');
-        
+
+    }
+
+    changing = (event) => {
     }
 
     render() {
@@ -34,11 +49,11 @@ class Courses extends Component {
                     <form onSubmit={this.addCourse}>
                         <div className="mb-3 mt-3">
                             <label className="form-label">Course Name:</label>
-                            <input type="text" className="form-control" name="email" />
+                            <input type="text" className="form-control" name="email" onChange={event => this.setState({courseName: event.target.value})}/>
                         </div>
                         <div className="mb-3">
                             <label for="pwd" className="form-label">Course Description:</label>
-                            <textarea type="text" className="form-control" name="pswd" />
+                            <textarea type="text" className="form-control" name="pswd" onChange={event => this.setState({courseDesc: event.target.value})}/>
                         </div>
                         <button type="submit" className="btn btn-primary">Submit</button>
                     </form>
